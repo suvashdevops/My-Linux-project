08fde85e0e47803c3d6f7350c4230c7c16ff75ac
suvash.devop <suvash.devops@gmail.com>
2026-04-04 22:19:22 +0545

    add comments

---
+++ 02_comments.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# single line comment 
+
+echo "hello"
+
+# Multiple line comments
+
+
+<< comments
+
+===========================
+comments
+
--- README.md
+++ README.md
@@ -43,3 +43,4 @@ your comment here
 
 comment
 
+
